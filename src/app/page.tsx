5ee7d8ceb03f19705742b06c1100e2014ec7ac51
andresc4f <164497480+goalcasor@users.noreply.github.com>
2024-08-20 17:03:33 +0200

    init metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/home/home.module.scss'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <video  autoPlay loop muted playsInline>
+      <video autoPlay loop muted playsInline>
       <source src='/video/logo_consultoria.mp4' type="video/mp4"></source>
       </video>
     </main>
